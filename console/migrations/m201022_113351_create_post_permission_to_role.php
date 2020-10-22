2bb4f542e1599bb1b9e427d10e8cb198535b9b33
s6002041620055 <48791737+aumcontrol@users.noreply.github.com>
2020-10-22 19:34:19 +0700

    Update m201022_113351_create_post_permission_to_role.php

--- console/migrations/m201022_113351_create_post_permission_to_role.php
+++ console/migrations/m201022_113351_create_post_permission_to_role.php
@@ -48,48 +48,28 @@ class m201022_113351_create_post_permission_to_role extends Migration
         echo "m201022_113351_create_post_permission_to_role cannot be reverted.\n";
         $auth = Yii::$app->authManager;
 
-        //Permission
-        $listPost = $auth->getPermission('post-index');
-        if($listPost){
-            $auth->remove($listPost);
-        }
-
-        $createPost = $auth->getPermission('post-create');
-        if($createPost){
-            $auth->remove($createPost);
-        }
-
-        $deletePost = $auth->getPermission('post-delete');
-        if($deletePost){
-            $auth->remove($deletePost);
-        }
-
-        $updatePost = $auth->getPermission('post-update');
-        if($deletePost){
-            $auth->remove($deletePost);
-        }
-
-        $viewPost = $auth->getPermission('post-view');
-        if($viewPost){
-            $auth->remove($viewPost);
-        }
-
-
-        //Role
-        // $author = $auth->getPermission('post-update');
-        // if($author){
-        //     $auth->remove($author);
-        // }
-
-        // $admin = $auth->getPermission('author');
-        // if($admin){
-        //     $auth->remove($admin);
-        // }
-
-        // $superAdmin = $auth->getPermission('admin');
-        // if($superAdmin){
-        //     $auth->remove($superAdmin);
-        // }
+         // get role
+         $author = $auth->getRole('author');
+         $admin = $auth->getRole('admin');
+         $superAdmin = $auth->getRole('super-admin');
+ 
+         //get Permission
+         $listPost = $auth->getPermission('post-index');
+         $createPost = $auth->getPermission('post-create');
+         $deletePost = $auth->getPermission('post-delete');
+         $updatePost = $auth->getPermission('post-update');
+         $viewPost = $auth->getPermission('post-view');
+ 
+         //assign
+         $auth->removeChild($author,$createPost);
+         $auth->removeChild($author,$listPost);
+         $auth->removeChild($author,$viewPost);
+         $auth->removeChild($author,$updatePost);
+ 
+         $auth->removeChild($admin,$author);
+ 
+         $auth->removeChild($superAdmin,$admin);
+         $auth->removeChild($superAdmin,$deletePost);
 
         return true;
     }
